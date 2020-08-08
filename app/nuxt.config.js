d59a6f3c08d510925150e96de234ebf8719a8d48
Robinson <a.robinsonsilvester@gmail.com>
2020-08-08 18:28:54 +0530

    mild update

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -88,9 +88,9 @@ export default {
   */
 	axios: {
         // baseURL: 'http://armsbackend.localhost'
-		// baseURL: 'http://localhost:1337'
+		 baseURL: 'http://localhost:1338'
 		//  baseURL: 'https://genie2.rotihouselao.com'
-    baseURL: 'http://14.139.159.112:1338'
+    // baseURL: 'http://14.139.159.112:1338'
 	},
 
   /*
